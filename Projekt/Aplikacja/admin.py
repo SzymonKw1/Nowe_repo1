09eb4e554b816d3d0853a98ded7a744d5e1e0aaa
Szymon <skwiatkowski3@st.swps.edu.pl>
2024-12-03 10:46:11 +0000

    3.12 laby

--- Projekt/Aplikacja/admin.py
+++ Projekt/Aplikacja/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Team, Person
+from .models import Team, Person, Stanowisko, Osoba
 
 admin.site.register(Team)
 admin.site.register(Person)
+admin.site.register(Stanowisko)
+
+
 # Register your models here.
+class OsobaAdmin(admin.ModelAdmin):
+    @admin.display(description= "Stanowisko (ID)")
+    def stanowisko_with_id(self, obj):
+        if obj.stanowisko :
+            return f'{obj.stanowisko} ({obj.stanowisko.id})'
+        return 'Brak stanowiska'
+    list_display =["imie", "nazwisko", "plec", "stanowisko_with_id", "data_dodania"]
+
+admin.site.register(Osoba, OsobaAdmin)
--- Projekt/Aplikacja/models.py
+++ Projekt/Aplikacja/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
@@ -29,3 +29,41 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=80, blank = False, null = False)
+    opis = models.TextField(blank = False, null = False)
+    def __str__(self):
+        return self.nazwa
+    class Meta:
+        verbose_name="Stanowisko"
+        verbose_name_plural="Stanowiska"
+
+PLCIE = models.IntegerChoices('PLEC', 'Kobieta Mężczyzna Inna')
+class Osoba(models.Model):
+    PLEC_CHOICES = (
+        ("K", "Kobieta"),
+        ("M", "Mężczyzna"),
+        ("I", "Inna"),
+    )
+    
+    imie = models.CharField(max_length=40, blank = False, null = False)
+    nazwisko = models.CharField(max_length=60, blank = False, null = False)
+    plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
+    stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
+    data_dodania = models.DateField(auto_now_add = True, editable = False)
+    
+    def __str__(self):
+        return f'{self.imie} {self.nazwisko}' 
+
+    
+    
+    
+    class Meta:
+        ordering =["nazwisko"]
+        verbose_name="Osoba"
+        verbose_name_plural="Osoby"
+
+
+
+
+
